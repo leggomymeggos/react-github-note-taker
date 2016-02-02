dfbab979ac8528676accaff9cc5ac085db1e0ac1
Megan Gaebler <mgaebler@pivotal.io>
2016-02-02 16:14:43 -0600

    Print random names. Update names with a click.

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
-    render(){
-        return <p>This is react with HTML!</p>
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: chance.first()
+        }
+    }
+
+    buttonClicked(){
+        const newState = {
+            name: chance.first()
+        };
+
+        this.setState(newState);
+    }
+
+    render() {
+        return <div>
+            <p>{this.props.message}</p>
+            <p>The name of the reload is: {this.state.name}</p>
+            <button onClick={this.buttonClicked.bind(this)}>click me</button>
+        </div>
     }
 }
 
